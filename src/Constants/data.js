0b3e27b81e5c611780e6d3ca941aa5461dfd75e5
souroursahar2020 <sahar.kachoukh@gmail.com>
2020-10-29 16:45:26 +0100

    first commit

---
+++ src/Constants/data.js
@@ -0,0 +1,45 @@
+export const Movie = [
+  {
+    title: "Aladin",
+    description: "Aladdin est un film d'aventure fantastique    ",
+    posterUrl:
+      "https://fr.web.img3.acsta.net/pictures/19/04/19/09/29/4029006.jpg",
+    rate: 5,
+  },
+  {
+    title: "THE GIVER",
+    description:
+      "Dans le futur, les émotions ont été supprimées grâce à l'effacement de toute trace d'histoire. Tous les individus sont formatés pour se comporter de façon préétablie. Le dépositaire de la mémoire est la seule personne qui peut se souvenir du passé. Un jour, c'est au jeune Jonas, 18 ans, de prendre la relève et ainsi d'apprendre les lourds secrets de l'humanité.  ",
+    posterUrl:
+      "https://resizing.flixster.com/QMgr5JDVqaFI_TvHl95tDvm8m5Y=/300x300/v2/https://flxt.tmsimg.com/assets/p10479443_p_v10_ac.jpg",
+
+    rate: 4,
+  },
+  {
+    title: "I am Mother",
+    description:
+      " Depuis sa naissance, une adolescente est élevée par un robot « Mère » jusqu'à ce qu’une inconnue venant de l’extérieur pénètre dans le bunker automatique et détruise les plans futurs de *La Mère* pour *Fille*",
+
+    posterUrl:
+      "https://fr.web.img3.acsta.net/pictures/19/05/09/22/29/3613535.jpg",
+
+    rate: 3,
+  },
+
+  {
+    title: "Zoe",
+    description: "Zoe est un film de science-fiction romantique américain  ",
+    posterUrl: "https://imgsrc.cineserie.com/2018/03/1300609.jpg?ver=1",
+
+    rate: 3,
+  },
+
+  {
+    title: "The Uninvited",
+    description:
+      "Anna sort de l'hôpital psychiatrique pour retrouver sa famille en la personne de sa sœur Alex et de son père car sa mère, malade, a péri dans l'incendie de leur maison. Mais voilà, d'étranges apparitions font dire à Anna que la nouvelle compagne de leur père, et au passage infirmière de feue sa mère, aurait tué cette dernière pour avoir la mainmise sur la famille. Confortée par Alex, Anna se sent de plus en plus en danger face à cette marâtre et décide donc de réagir.      ",
+    posterUrl:
+      "https://upload.wikimedia.org/wikipedia/en/thumb/6/60/The_Uninvited_%282009_film%29.jpg/215px-The_Uninvited_%282009_film%29.jpg",
+    rate: 2,
+  },
+];
